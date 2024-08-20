f4158d9ff9f3a40d63463eca932b75974b83f20c
macarenacarrica <macarenacarrica23@gmail.com>
2024-08-19 21:07:47 -0300

    cambié el error-message por un alert

--- js/login.js
+++ js/login.js
@@ -2,11 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('loginButton');
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
-    const errorMessage = document.getElementById('error-message');
 
     loginButton.addEventListener('click', () => {
-        // Limpia el mensaje de error
-        errorMessage.textContent = '';
+    
 
         // Obtiene los valores de los campos
         const username = usernameInput.value.trim();
@@ -14,12 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Verifica si los campos están vacíos
         if (!username || !password) {
-            errorMessage.textContent = 'Por favor, complete todos los campos.';
+            alert('Por favor, complete todos los campos.'); // Muestra una alerta si los campos están vacíos
             return; // Detiene la ejecución del código si los campos están vacíos
         }
 
         // Si la validación pasa, redirige a la página de inicio
-        window.location.href = 'index.html';
+        window.location.href = '.../index.html';
     });
 });
 
