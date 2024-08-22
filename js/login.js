ecca00eb74954f9f9943dc787308a7d48e9f5491
soledadpadilla <soledad04padilla@gmail.com>
2024-08-21 21:58:41 -0300

    Update login.js
    Guardar sesión y redirigir a login

--- js/login.js
+++ js/login.js
@@ -1,28 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.getElementById("loginForm");
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
-    const alertSuccess = document.getElementById("alert-success");
-    const alertDanger = document.getElementById("alert-danger");
+    // Verifica si ya se ingresó
+    if (localStorage.getItem('sesionIniciada') === 'true') {
+        // Si la sesión está ingresada, redirige al inicio
+        window.location.href = "index.html";
+    } else {
+        // Si no hay sesión iniciada, proceder con la lógica del formulario
+        const loginForm = document.getElementById("loginForm");
+        const username = document.getElementById("username");
+        const password = document.getElementById("password");
+        const alertSuccess = document.getElementById("alert-success");
+        const alertDanger = document.getElementById("alert-danger");
 
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+        loginForm.addEventListener("submit", function(event) {
+            event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
 
-        // Ocultar alertas
-        alertSuccess.classList.remove("show");
-        alertDanger.classList.remove("show");
+            // Ocultar alertas
+            alertSuccess.classList.remove("show");
+            alertDanger.classList.remove("show");
 
-        if (username.value.trim() === "" || password.value.trim() === "") {
-            // Mostrar alerta de error
-            alertDanger.classList.add("show");
-        } else {
-            // Mostrar alerta de éxito
-            alertSuccess.classList.add("show");
-              // Redirigir a index.html después de un corto retraso
-              setTimeout(function() {
-                window.location.href = "index.html";
-            }, 1500); 
-        }
-    });
+            if (username.value.trim() === "" || password.value.trim() === "") {
+                // Mostrar alerta de error
+                alertDanger.classList.add("show");
+            } else {
+                // Guardar sesión al iniciar correctamente
+                localStorage.setItem('sesionIniciada', 'true');
+                
+                // Mostrar alerta de éxito
+                alertSuccess.classList.add("show");
+                
+                // Redirigir a index.html después de un corto retraso
+                setTimeout(function() {
+                    window.location.href = "index.html";
+                }, 1500); 
+            }
+        });
+    }
 });
-
